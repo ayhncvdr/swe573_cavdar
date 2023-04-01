79dc82282b9a39f6abcfadc73614a3134e063d4a
ayhncvdr <ayhan.cavdar@boun.edu.tr>
2023-04-01 15:16:23 +0300

    signup request and signup html page is created #14

--- core/models.py
+++ core/models.py
@@ -9,12 +9,11 @@ User = get_user_model()
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     id_user = models.IntegerField()
-    # INFO: these will be from django auth
-    # first_name = models.CharField(max_length=100)
-    # last_name = models.CharField(max_length=100)
-    # username = models.CharField(max_length=50, unique=True)
-    # email = models.EmailField(unique=True)
-    # password = models.CharField(max_length=128)
+    first_name = models.CharField(max_length=100, default="")
+    last_name = models.CharField(max_length=100,  default="")
+    username = models.CharField(max_length=50, unique=True, default="")
+    email = models.EmailField(unique=True, default="")
+    password = models.CharField(max_length=128, default="")
     profile_image = models.ImageField(
         upload_to='profile_images', default='blank-profile-picture.png')
     bio = models.TextField(blank=True)
--- core/views.py
+++ core/views.py
@@ -1,8 +1,49 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+
+from .models import Profile
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('<h1>Welcome to Memorycloud</h1>')
+    return render(request, 'index.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        firstname = request.POST['First Name']
+        lastname = request.POST['Last Name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already exists')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username already exists')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(
+                    username=username, email=email, password=password, first_name=firstname, last_name=lastname)
+                user.save()
+
+                # TODO log user in and redirect to settings page
+
+                # create a profile object for the new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id, first_name=user_model.first_name,
+                                                     last_name=user_model.last_name, username=user_model.username, email=user_model.email, password=user_model.password)
+                new_profile.save()
+                # TODO redirect sign in later
+                return redirect('signup')
+        else:
+            messages.info(request, 'Passwords do not match')
+            return redirect('signup')
+
+    else:
+        return render(request, 'signup.html')
