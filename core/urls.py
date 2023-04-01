b8bab01dc8be1667f464b3f8a7c26eb6175f63bc
ayhncvdr <ayhan.cavdar@boun.edu.tr>
2023-04-01 15:52:12 +0300

    sign in request and html page is created #15

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('signup', views.signup, name='signup')
-
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin')
 ]
