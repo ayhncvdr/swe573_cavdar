832535ec29ab039dca271e0b72d5b80c0b7dd02a
ayhncvdr <ayhan.cavdar@boun.edu.tr>
2023-05-25 15:25:42 +0300

    unit tests are added for the rest of the requests #41

--- core/tests.py
+++ core/tests.py
@@ -4,6 +4,8 @@ import uuid
 from django.db import IntegrityError
 from django.test import TestCase, Client, RequestFactory
 from django.urls import reverse
+
+from memorycloud.settings import AUTH_PASSWORD_VALIDATORS
 from .models import Follower, Like, Story, Tag, Location, Comment
 from .models import Profile
 from .forms import StoryForm
@@ -181,45 +183,281 @@ class DiscoverTestCase(TestCase):
         # Assert that the user object is passed to the context
         self.assertEqual(response.context['user_object'], self.user)
 
+
 class NewPostTestCase(TestCase):
     def setUp(self):
-        self.client = Client()
         self.user = User.objects.create_user(
-            username='testuser',
-            email='testuser@example.com',
-            password='testpassword'
-        )
-        self.profile = Profile.objects.create(user=self.user, email='testuser@example.com', username='testpassword')
+            username='testuser', email='testuser@example.com', password='testpass')
+        self.user_profile = Profile.objects.create(user=self.user)
+        self.client.login(username='testuser', password='testpass')
+        self.story = Story.objects.create(
+            title='Test Title', user=self.user, content='Test Content', date_format=1)
 
     def test_new_post(self):
-        # Authenticate the test client
-        self.client.login(username='testuser', password='testpassword')
-
-        # Make a POST request to the newPost view
         url = reverse('newpost')
         data = {
             'title': 'Test Title',
             'content': 'Test Content',
-            'date_option': 'exact_date',  # Provide the appropriate value for the date_option field
-            # Include other required fields and their values here
+            'date_option': 'exact_date',
+            'exact_date': '2022-01-01',
+            'tags': '[{"value": "Test Tag"}]',
+            'files': '',
+            'features': ''
         }
-
         response = self.client.post(url, data)
-
-        # Assert the response status code is a redirect (302)
         self.assertEqual(response.status_code, 302)
-
-        # Assert that a new Story object is created in the database
-        self.assertEqual(Story.objects.count(), 0)
-
-        # Add additional assertions as needed to verify the behavior of the function
+        story_filter = Story.objects.filter(title='Test Title').first()
+        self.assertIsNotNone(story_filter)
+        self.assertEqual(story_filter.content, 'Test Content')
 
 
+class LikePostViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='testuser',  email='testuser@example.com', password='testpass')
+        self.user_profile = Profile.objects.create(user=self.user)
+        self.story = Story.objects.create(
+            title='Test Story', user=self.user, content='Test Content', date_format=1)
+        self.like = Like.objects.create(
+            story=self.story, user=self.user)
 
+    def test_like_post(self):
+        self.client.login(username='testuser', password='testpass')
+        url = reverse('like-post')
+        response = self.client.get(url, {'story_id': self.story.id})
+        self.assertEqual(response.status_code, 302)
+        like_filter = Like.objects.filter(
+            story=self.story, user=self.user).first()
+        if like_filter is None:
+            new_like = Like.objects.create(story=self.story, user=self.user)
+            new_like.save()
+            self.story.no_of_likes += 1
+            self.story.is_liked_by_current_user = True
+            self.story.save()
+        else:
+            like_filter.delete()
+            self.story.no_of_likes -= 1
+            self.story.is_liked_by_current_user = False
+            self.story.save()
+        self.assertRedirects(response, '/')
 
+class CommentPostTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser', email='testuser@example.com', password='testpass')
+        self.story = Story.objects.create(
+            title='Test Story', content='Test Content', user=self.user, date_format=1)
+        self.client.login(username='testuser', password='testpass')
 
+    def test_comment_post(self):
+        url = reverse('comment-post')
+        data = {'comment': 'Test Comment', 'story_id': self.story.id}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 302)
+        comment_filter = Comment.objects.filter(
+            story=self.story, user=self.user).first()
+        self.assertIsNotNone(comment_filter)
+        self.assertEqual(comment_filter.content, 'Test Comment')
 
-  
-        
+class DeleteStoryTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser', email='testuser@example.com', password='testpass')
+        self.story = Story.objects.create(
+            title='Test Story', content='Test Content', user=self.user, date_format=1)
+        self.client.login(username='testuser', password='testpass')
 
+    def test_delete_story(self):
+        url = reverse('delete-story')
+        data = {'story_id': self.story.id}
+        response = self.client.get(url, data)
+        self.assertEqual(response.status_code, 302)
+        story_filter = Story.objects.filter(id=self.story.id).first()
+        self.assertIsNone(story_filter)
+
+class SearchTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+        username='testuser', email='testuser@example.com', password='testpass')
+        self.user_profile = Profile.objects.create(user=self.user)
+        self.client.login(username='testuser', password='testpass')
+
+    def test_search(self):
+        url = reverse('search')
+        response = self.client.get(url, {'query': 'Test Query'})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'search.html')
+        self.assertContains(response, 'Test Query')
+
+class PostDetailedTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+        username='testuser', email='testuser@example.com', password='testpass')
+        self.user_profile = Profile.objects.create(user=self.user)
+        self.client.login(username='testuser', password='testpass')
+
+    def test_post_detailed(self):
+        story = Story.objects.create(title='Test Title', content='Test Content', user=self.user, date_format=1)
+        url = reverse('postdetailed') + f'?story_id={story.id}'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'postdetailed.html')
+        self.assertContains(response, 'Test Title')
+        self.assertContains(response, 'Test Content')
+
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+        username='testuser', email='testuser@example.com', password='testpass')
+        self.user_profile = Profile.objects.create(user=self.user)
+        self.client.login(username='testuser', password='testpass')
+
+    def test_profile(self):
+        user = User.objects.create_user(
+        username='testuser2', email='testuser2@example.com', password='testpass')
+        url = reverse('profile', args=[user.username])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'profile.html')
+        self.assertContains(response, user.username)
+
+class FollowTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+        username='testuser', email='testuser@example.com', password='testpass')
+        self.user_profile = Profile.objects.create(user=self.user)
+        self.client.login(username='testuser', password='testpass')
+
+    def test_follow(self):
+        user = User.objects.create_user(
+        username='testuser2', email='testuser2@example.com', password='testpass')
+        url = reverse('follow')
+        data = {'follower': self.user.username, 'user': user.username}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/profile/' + user.username)
+        self.assertTrue(Follower.objects.filter(follower=self.user, user=user).exists())
+
+class UsersFollowersTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+        username='testuser', email='testuser@example.com', password='testpass')
+        self.user_profile = Profile.objects.create(user=self.user)
+        self.client.login(username='testuser', password='testpass')
+
+    def test_users_followers(self):
+        user = User.objects.create_user(
+        username='testuser2', email='testuser2@example.com', password='testpass')
+        url = reverse('usersfollowers') + f'?user_name={user.username}'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'followers.html')
+
+class UsersFollowingTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+        username='testuser', email='testuser@example.com', password='testpass')
+        self.user_profile = Profile.objects.create(user=self.user)
+        self.client.login(username='testuser', password='testpass')
+
+    def test_users_following(self):
+        user = User.objects.create_user(
+        username='testuser2', email='testuser2@example.com', password='testpass')
+        follower1=Follower.objects.create(user=user, follower=self.user)
+        url = reverse('usersfollowing') + f'?user_name={self.user.username}'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertListEqual(list(response.context['followers']), [follower1])
+        self.assertEqual(response.context['current_user'], self.user)
+
+class UsersLikedTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+        username='testuser', email='testuser@example.com', password='testpass')
+        self.user_profile = Profile.objects.create(user=self.user)
+        self.client.login(username='testuser', password='testpass')
+
+    def test_users_liked(self):
+        story = Story.objects.create(title='Test Story', content='This is a test story.',user=self.user)
+        like1 = Like.objects.create(story=story, user=self.user)
+        url = reverse('usersliked') + f'?story_id={story.id}'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['story'], story)
+        self.assertListEqual(list(response.context['likes']), [like1])
+
+class UsersCommentedTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+        username='testuser', email='testuser@example.com', password='testpass')
+        self.user_profile = Profile.objects.create(user=self.user)
+        self.client.login(username='testuser', password='testpass')
+
+    def test_users_commented(self):
+        story = Story.objects.create(title='Test Story', content='This is a test story.', user=self.user)
+        comment1 = Comment.objects.create(story=story, user=self.user, content='This is a test comment.')
+        url = reverse('userscommented') + f'?story_id={story.id}'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['story'], story)
+        self.assertListEqual(list(response.context['comments']), [comment1])
+
+class SettingsTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+        username='testuser', email='testuser@example.com', password='testpass')
+        self.user_profile = Profile.objects.create(user=self.user)
+        self.client.login(username='testuser', password='testpass')
+
+    def test_settings(self):
+        url = reverse('settings')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+        data = {
+        'bio': 'This is a test bio.',
+        'first_name': 'Test',
+        'last_name': 'User'
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('index'))
+
+        user_profile = Profile.objects.get(user=self.user)
+        self.assertEqual(user_profile.bio, data['bio'])
+        self.assertEqual(user_profile.first_name, data['first_name'])
+        self.assertEqual(user_profile.last_name, data['last_name'])
+
+class SignupTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_signup(self):
+        url = reverse('signup')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+        data = {
+        'First Name': 'Test',
+        'Last Name': 'User',
+        'username': 'testuser',
+        'email': 'testuser@example.com',
+        'password': 'testpass',
+        'password2': 'testpass'
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('settings'))
+
+        user = User.objects.get(username=data['username'])
+        self.assertEqual(user.email, data['email'])
+        self.assertEqual(user.first_name, data['First Name'])
+        self.assertEqual(user.last_name, data['Last Name'])
+
+        profile = Profile.objects.get(user=user)
+        self.assertEqual(profile.username, data['username'])
+        self.assertEqual(profile.email, data['email'])
+        self.assertEqual(profile.first_name, data['First Name'])
+        self.assertEqual(profile.last_name, data['Last Name'])
 
